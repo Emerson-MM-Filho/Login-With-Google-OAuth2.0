c0901cd9e3625c877056a21172a5209d04a6100f
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-10-17 23:57:09 -0400

    import authenticate middleware and create the authenticated route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import { authMiddleware } from './authMiddleware'
 
 const app = express();
 
 app.use(express.static(__dirname + '/../public'))
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.sendFile('index.html');
 });
+
+app.get('/authenticated', authMiddleware, (_req, res) => {
+    const userId = res.locals.userId;
+    res.json({
+        value: 'Protected info',
+        userId
+    })
+})
 
 app.listen(9092, () => console.log('Running in localhost:3000!'));
