98e42bab2a48e1f2a3b2521678d7c67478607166
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-10-17 21:13:52 -0400

    change app port and add the main route to the application

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => console.log('Running in localhost:3000!'));
+app.use(express.static(__dirname + '/../public'))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+app.listen(9092, () => console.log('Running in localhost:3000!'));
