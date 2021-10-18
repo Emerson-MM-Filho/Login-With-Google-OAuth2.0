fd0c35536661e7a8c58d64fdd1dba7ddb440db77
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-10-18 10:01:39 -0400

    import already authenticated middleware and fix routes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import express from 'express';
-import { authMiddleware } from './authMiddleware'
+import express, { Request, Response } from 'express';
+import { authMiddleware } from './authMiddleware';
+import { alreadyAuthenticatedMiddleware } from './alreadyAuthenticatedMiddleware';
 
 const app = express();
 
 app.use(express.static(__dirname + '/../public'))
+app.use(express.json());
 
-app.get('/', (_req, res) => {
+app.post('/', (_req, res) => {
     res.sendFile('index.html');
 });
 
-app.get('/authenticated', authMiddleware, (_req, res) => {
-    const userId = res.locals.userId;
-    res.json({
-        value: 'Protected info',
-        userId
-    })
+app.post('/login', authMiddleware)
+app.get('/authenticated', alreadyAuthenticatedMiddleware, ( _req: Request, res: Response) => {
+  const userId = res.locals['userid'];
+
+  res.json({
+      value: 'Protected Route',
+      userId
+  })
 })
 
 app.listen(9092, () => console.log('Running in localhost:9092!'));
